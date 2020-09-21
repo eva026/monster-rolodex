8187dc72004cebec145e494666b0606eea4213cf
Eva Wang <eva026.wangwang@gmail.com>
2020-09-21 15:11:52 +0800

    Updates

---
+++ precache-manifest.36bc53e08f41d08572b817ab6c8be3e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73be007c71532c34f75c2132c1ee9c8a",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f5c63cea69883494b17f",
+    "url": "/monster-rolodex/static/css/main.ade49eda.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monster-rolodex/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c63cea69883494b17f",
+    "url": "/monster-rolodex/static/js/main.e893f078.chunk.js"
+  },
+  {
+    "revision": "b70a9c55c535ea67c1fc",
+    "url": "/monster-rolodex/static/js/runtime-main.80f93c1b.js"
+  }
+]);
